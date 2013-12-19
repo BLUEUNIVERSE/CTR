ba48eb3fc1c80c0a0ccd58d3aae867c8bb6bb5c3
AniketShark <aniket.kayande@gmail.com>
2013-12-19 01:43:13 -0500

    1> power up guided missile.

---
+++ CTR/Assets/Scripts/GamplayScripts/Powers/Power.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Power : MonoBehaviour 
+{
+    public string m_sName;
+    public PowerType m_Type;
+    public GameObject m_SpecialEffect; 
+    public AudioClip m_SoundEffect;
+    public Transform m_Target;
+
+    virtual public void PlaySoundEffect()
+    {
+        if(m_SoundEffect != null)
+        {
+            GetComponent<AudioSource>().PlayOneShot(m_SoundEffect);
+        }
+    }
+
+    virtual public void SpawnSpecialEffect()
+    {
+
+    }
+
+}
+
+public enum PowerType 
+{
+    Projectile,
+    GlobalHit,
+    Advantage
+}
