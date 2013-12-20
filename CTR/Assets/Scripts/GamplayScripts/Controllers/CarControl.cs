bb2f3e447556b5f14e74ef40cf2739b52da209fc
AniketShark <aniket.kayande@gmail.com>
2013-12-20 15:28:52 -0500

    Added 4 new scripts to game

--- CTR/Assets/Scripts/GamplayScripts/Controllers/CarControl.cs
+++ CTR/Assets/Scripts/GamplayScripts/Controllers/CarControl.cs
@@ -11,7 +11,12 @@ public class CarControl : CarMechanicsBase
 {
     Vector3 dir = Vector3.zero;
     public InputManager m_InputManager;
-    protected PlayerStats m_PlayerStats;
+    public CarStats m_CarStats;
+    
+    private float m_fNitroCoolDownTime;
+    private float m_fTotalTime;
+    private float m_fSaveTime;
+    
 
     void Start()
     {
@@ -29,13 +34,31 @@ public class CarControl : CarMechanicsBase
 		float inputTorque = m_InputManager.m_fThrottleValue;
         Vector3 input = new Vector3(inputSteer,inputTorque,0);
        	CarUpdate(input);
-       
+        
     }
 
     void OnGUI()
     {
         //string str  = "X = " + dir.x + "  Y = " + dir.y + "  Z = " + dir.z;
         //GUI.Button(new Rect(5, 5, 1024, 50),str);
+    }
+
+
+    void NitroBoost()
+    {
+        m_fCurrentEngineTorque = m_fNormalEngineTorque + m_fAdditionalNitroTorque;
+        m_fSaveTime = Time.time;
+        InvokeRepeating("NitroCooldown", m_CarStats.m_NitroPayload, Time.deltaTime);
+    }
+
+    void NitroCooldown()
+    {
+        m_fCurrentEngineTorque = Mathf.Lerp(m_fCurrentEngineTorque, m_fNormalEngineTorque, Time.deltaTime);
+        m_fTotalTime += Time.deltaTime; 
+        if(m_fTotalTime >= m_fNitroCoolDownTime)
+        {
+            CancelInvoke("NitroCooldown");
+        }
     }
 
 }
